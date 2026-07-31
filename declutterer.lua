f3322ed7c2cd633b79a78f8b3f09dfb469b0835d
Pat Colyn <13235826+patcolyn@users.noreply.github.com>
2026-07-31 18:47:39 +0100

    Add Declutterer

---
+++ declutterer.lua
@@ -0,0 +1,16 @@
+-- Declutterer
+
+function onLoad()
+    if Info.name ~= "MTG EDH 4-player (π)" then return end
+    local declutterObject = getObjectFromGUID("e40450")
+    if declutterObject then declutterObject.call("closeSelf") end
+
+    -- Custom star field
+    Backgrounds.setCustomURL("https://steamusercontent-a.akamaihd.net/ugc/14883842009767733466/FBF8E496D1B826BEC2D52A9E57AE275F6B486061/")
+    
+    MusicPlayer.setPlaylist({})
+    -- Empty audio clip
+    MusicPlayer.setCurrentAudioclip({url="https://steamusercontent-a.akamaihd.net/ugc/10070388021481426706/62D820A785B8DF98FAA1B297BC6A80B4D21EB414/",title=" "})
+
+    --self.destruct()
+end
